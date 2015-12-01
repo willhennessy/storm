e9a98992c3f516f1fbf3c234005883e1a5b3a097
Will Hennessy <hennessywill@gmail.com>
2015-11-30 20:52:21 -0600

    Present paginate ideas from database

--- javascripts/clock.js
+++ javascripts/clock.js
@@ -1,5 +1,7 @@
 var min = 1;
 var sec = 11;
+var ideas = null; // array of ideas from db
+var numIdeasCompleted = 0;
 
 function isWithinOneHour(now, start) {
   var diff = start.getTime() - now.getTime();
@@ -33,9 +35,57 @@ function initIdeationClock(startDate, startTime, duration) {
 }
 
 function initDiscussionClock(startDate, startTime, duration) {
-  min = 10;
-  sec = 0;
-  startClock();
+  min = 0;
+  sec = 5;
+
+  $("#clock").html(getTimeString());
+  updateDiscussionClock(); // run once to start
+  intervalID = setInterval(updateDiscussionClock,1000);
+}
+
+function updateDiscussionClock() {
+  if (min == 0 && sec == 0 && ideas === null) {
+  	fetchIdeas();
+  } else if (min == 0 && sec == 0 && !isDiscussionDone()) { 
+    displayNextIdea();
+    min = 0;
+    sec = 10;
+    $("#clock").html(getTimeString());
+  } else if (min == 0 && sec == 0 && isDiscussionDone()) {
+  	finishStage();
+  } else {
+	sec -= 1;
+	if (sec < 0) {
+	  min -= 1;
+	  sec = 59;
+	}
+	$("#clock").html(getTimeString());
+  }
+}
+
+/* Loads the ideas from the db and assigns them to 'ideas' var */
+function fetchIdeas() {
+  var key = getFirebaseKey();
+  var ref = new Firebase(getSessionUrl(key));
+  ref.once("value", function(snapshot) {
+    var session = snapshot.val();
+    ideas = session.ideas;
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
+
+/* Replace the title and description with the next one */
+function displayNextIdea() {
+  var currIdea = ideas[numIdeasCompleted];
+  $("#title").html(currIdea.title);
+  $("#description").html(currIdea.description);
+  numIdeasCompleted += 1;
+}
+
+/* Returns true if every topic has been discussed */
+function isDiscussionDone() {
+  return numIdeasCompleted > ideas.length;
 }
 
 /* route the appropriate finish function */
